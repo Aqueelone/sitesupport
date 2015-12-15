69f4f6a1a2b727087e6378724082b56552d39585
Sergy Nazarevich <aqueelone@gmail.com>
2015-12-15 02:56:25 +0200

    [I2] -- added necessary and sufficient gems to GemFile -- moved to PostgreSQL -- init test workflow: cucumber, rspec, capybara, jasmine -- init helpers workflow: guard with rubocop, jshintrb, haml-lint, pronto -- init TravisCI support -- developed Acceptance Tests via capybara/rspec

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,24 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "email_spec"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+config.before(:suite) do
+  DatabaseCleaner.strategy = :truncation
+end
+
+config.before(:each) do
+  DatabaseCleaner.clean
+end
+
+RSpec.configure do |config|
+  config.include(EmailSpec::Helpers)
+  config.include(EmailSpec::Matchers)
 end
