55c631297c20219ec842a2ff235ac716b6c8e704
Sergy Nazarevich <aqueelone@gmail.com>
2015-12-20 03:54:43 +0200

    [I3] added generated StaticPages controller and views for it added tests for StaticPages added code for StaticPages veiws fixed Application layout added message, menu and footer partials added full design for StaticPages refactored design and partials for propertly work

---
+++ spec/views/static_pages/about.html.haml_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "static_pages/about.html.haml", type: :view do
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/about'
+      expect(page).to have_content('About')
+    end
+  end
+end
